30510c0b6ae78b44adefc63618e53a39809fb171
Akuoiqn <115793374+Akuoiqn@users.noreply.github.com>
2023-05-30 15:10:43 +0300

    Update haptics.ino

--- firmware/lucidgloves-firmware/haptics.ino
+++ firmware/lucidgloves-firmware/haptics.ino
@@ -6,18 +6,20 @@
   #include "Servo.h"
 #endif
 
-Servo pinkyServo;
-Servo ringServo;
-Servo middleServo;
-Servo indexServo;
-Servo thumbServo;
-
+//Servo pinkyServo;
+//Servo ringServo;
+//Servo middleServo;
+//Servo indexServo;
+//Servo thumbServo;
+int lowCheck = 0;
+int highCheck = 0;
+bool grabButton = 13;
 void setupServoHaptics(){
-  pinkyServo.attach(PIN_PINKY_MOTOR);
-  ringServo.attach(PIN_RING_MOTOR);
-  middleServo.attach(PIN_MIDDLE_MOTOR);
-  indexServo.attach(PIN_INDEX_MOTOR);
-  thumbServo.attach(PIN_THUMB_MOTOR);
+pinMode(PIN_PINKY_MOTOR,OUTPUT);
+pinMode(PIN_RING_MOTOR,OUTPUT);
+pinMode(PIN_MIDDLE_MOTOR,OUTPUT);
+pinMode(PIN_INDEX_MOTOR,OUTPUT);
+pinMode(PIN_THUMB_MOTOR,OUTPUT);
 }
 
 //static scaling, maps to entire range of servo
@@ -41,14 +43,111 @@ void dynScaleLimits(int* hapticLimits, float* scaledLimits){
   }
 }
 
-void writeServoHaptics(int* hapticLimits){
-  float scaledLimits[5];
-  scaleLimits(hapticLimits, scaledLimits);
-  if(hapticLimits[0] >= 0) thumbServo.write(scaledLimits[0]);
-  if(hapticLimits[1] >= 0) indexServo.write(scaledLimits[1]);
-  if(hapticLimits[2] >= 0) middleServo.write(scaledLimits[2]);
-  if(hapticLimits[3] >= 0) ringServo.write(scaledLimits[3]);
-  if(hapticLimits[4] >= 0) pinkyServo.write(scaledLimits[4]);
-}
+void writeServoHaptics(){
+  //int* hapticLimits^^
+//  float scaledLimits[5];
+//  scaleLimits(hapticLimits, scaledLimits);
 
+//while (analogRead(PIN_JOY_BTN)>=512){
+//  if (PIN_PINKY_MOTOR == LOW){
+//    continue;}
+//    else{
+//      digitalWrite(PIN_PINKY_MOTOR, LOW);
+//      }
+//  }
+//
+//  while (analogRead(PIN_JOY_BTN)<=511){
+//  if (PIN_PINKY_MOTOR == HIGH){
+//    continue;}
+//    else{
+//      digitalWrite(PIN_PINKY_MOTOR, HIGH);
+//      }
+//  }
+  
+}
+//  if(grabButton == true){
+//    if(fingerPos[0]>=511){
+//    digitalWrite(PIN_PINKY_MOTOR, LOW);}
+//    else{
+//    digitalWrite(PIN_PINKY_MOTOR, HIGH);}
+//
+//  if(fingerPos[1]>=511){
+//    digitalWrite(PIN_RING_MOTOR, LOW);}
+//    else{
+//    digitalWrite(PIN_RING_MOTOR, HIGH);}
+//
+//  if(fingerPos[2]>=511){
+//    digitalWrite(PIN_MIDDLE_MOTOR, LOW);}
+//    else{
+//    digitalWrite(PIN_MIDDLE_MOTOR, HIGH);}
+//
+//  if(fingerPos[3]>=511){
+//    digitalWrite(PIN_INDEX_MOTOR, LOW);}
+//    else{
+//    digitalWrite(PIN_INDEX_MOTOR, HIGH);}
+//
+//  if(fingerPos[4]>=511){
+//    digitalWrite(PIN_THUMB_MOTOR, LOW);}
+//    else{
+//    digitalWrite(PIN_THUMB_MOTOR, HIGH);}}
+//    else{
+//      digitalWrite(PIN_PINKY_MOTOR, HIGH);
+//      digitalWrite(PIN_RING_MOTOR, HIGH);
+//      digitalWrite(PIN_MIDDLE_MOTOR, HIGH);
+//      digitalWrite(PIN_INDEX_MOTOR, HIGH);
+//      digitalWrite(PIN_THUMB_MOTOR, HIGH);
+//      }
+//    }
+//  if(hapticLimits[0] >= 0) {
+//    thumbServo.write(scaledLimits[0]);
+////    if(PIN_THUMB_MOTOR == LOW){
+////    digitalWrite(PIN_THUMB_MOTOR, HIGH);}
+//    };
+//  if(hapticLimits[1] >= 0) {
+//    indexServo.write(scaledLimits[1]);
+////    if(PIN_INDEX_MOTOR == LOW){
+////  digitalWrite(PIN_INDEX_MOTOR, HIGH);}
+//  };
+//  if(hapticLimits[2] >= 0) {
+//    middleServo.write(scaledLimits[2]);
+////    if(PIN_MIDDLE_MOTOR == LOW){
+////    digitalWrite(PIN_MIDDLE_MOTOR, HIGH);}
+//};
+//  if(hapticLimits[3] >= 0) {
+//    ringServo.write(scaledLimits[3]);
+////    if(PIN_RING_MOTOR == LOW){
+////    digitalWrite(PIN_RING_MOTOR, HIGH);}
+//    };
+//  if(hapticLimits[4] >= 0) {
+//    pinkyServo.write(scaledLimits[4]);
+////    if(PIN_PINKY_MOTOR == LOW){
+////    digitalWrite(PIN_PINKY_MOTOR, HIGH);}
+//    };
+//}
+void stateMagnet(int* fingerPos) {
+
+  if (fingerPos[2]>=600){
+    if(PIN_THUMB_MOTOR == LOW) return;
+   if(lowCheck < 10){
+    lowCheck = lowCheck + 1;
+    }else{
+   digitalWrite(PIN_THUMB_MOTOR, LOW);
+   digitalWrite(PIN_INDEX_MOTOR, LOW);
+   digitalWrite(PIN_MIDDLE_MOTOR, LOW);
+   digitalWrite(PIN_RING_MOTOR, LOW);
+   digitalWrite(PIN_PINKY_MOTOR, LOW);
+   lowCheck = 0;
+   }}
+   else{
+    if (PIN_THUMB_MOTOR == HIGH) return; 
+     if(highCheck < 10){
+    highCheck = highCheck + 1;
+    }else{
+     digitalWrite(PIN_THUMB_MOTOR, HIGH);
+    digitalWrite(PIN_INDEX_MOTOR, HIGH);
+   digitalWrite(PIN_MIDDLE_MOTOR, HIGH);
+   digitalWrite(PIN_RING_MOTOR, HIGH);
+   digitalWrite(PIN_PINKY_MOTOR, HIGH);
+  highCheck = 0;
+   }}}
 #endif
